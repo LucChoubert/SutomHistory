d7b68f9cb39ecd5e88472cb78a9acdd09fb79cf8
Luc Choubert <luc.choubert@gmail.com>
2022-12-28 22:27:40 +0100

    multi-arch runCI with podman

--- runCI.sh
+++ runCI.sh
@@ -1,15 +1,26 @@
 #! /bin/bash
 
+set -x
+
+# This is to propagate RC
+set -o pipefail
+
 #Inspired from: https://github.com/fluxcd/helm-operator-get-started/blob/master/README.md
 
-repository="lucchoubert/sutomhistory"
+repository="docker.io/lucchoubert/sutomhistory"
 branch="master"
 version=""
 #Does not work on macos# commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
 commit=$(openssl rand -hex 8)
 
-last_version=$(docker image ls $repository | head -n 2 | tail -n 1 | sed 's/[^ ]*[ ]*\([^ ]*\).*/\1/')
-last_version="0.0"
+#last_version=$(podman image ls $repository | head -n 2 | tail -n 1 | sed 's/[^ ]*[ ]*\([^ ]*\).*/\1/')
+last_version=$(podman search --no-trunc --list-tags $repository | tail -n 1 | sed 's/[^ ]*[ ]*\([^ ]*\).*/\1/')
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    last_version="0.0"
+    echo "Error"
+fi
+
 major_version=$(echo $last_version | cut -d '.' -f 1)
 minor_version=$(echo $last_version | cut -d '.' -f 2)
 minor_version=$((minor_version+1))
@@ -20,7 +31,19 @@ image="${repository}:${version}"
 
 echo ">>>> Building image ${image} <<<<"
 
-docker build --build-arg RANDOMSTRING=${commit} --build-arg VERSION=${version} -t ${image} .
+# This is for debug
+#exit 0
 
-docker push ${image}
+# Following line is to build multi arch on with docker. Works well on Windows.
+#/home/luc/.docker/cli-plugins/docker-buildx build --platform linux/amd64,linux/arm64 -t ${image} --push .
+
+# Build the container for multi architecture with podman
+#podman build --build-arg RANDOMSTRING=${commit} --build-arg VERSION=${version} -t ${image} .
+#podman build --platform linux/arm64 --platform linux/amd64 --manifest docker.io/lucchoubert/sutomhistory:0.2 .
+podman build --platform linux/arm64 --platform linux/amd64 --manifest ${image} .
+
+# Pushing to the master Repo
+#podman push ${image}
+#podman manifest push docker.io/lucchoubert/sutomhistory:0.2 docker://docker.io/lucchoubert/sutomhistory:0.2
+podman manifest push ${image} docker://${image}
 
